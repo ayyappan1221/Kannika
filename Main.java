a06f2b67ad048ea2a913c2c3ff80b407a57d8626
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-14 07:50:54 +0530

    basic

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short s = 200;
+        int i = 1000;
+        long l = 123456789L;
+
+        float f = 10.5f;
+        double d = 99.99;
+
+        char c = 'A';
+        boolean flag = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(flag);
+    }
+}
---
+++ basicoutput.java
@@ -0,0 +1,6 @@
+public class basicoutput {
+    public static void main(String[] args) {
+        String name = "kannika";
+        System.out.println(name);
+    }
+}
