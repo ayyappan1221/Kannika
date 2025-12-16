631bc842fb532754c55204a91732b8ecf0564751
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-16 13:52:23 +0530

    Add Strong Number program to check if a number is a Strong Number

---
+++ Numarical promblem/Power.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int r = 1;
+
+        for (int i = 1; i <= b; i++) {
+            r = r * a;
+        }
+
+        System.out.println(r);
+        sc.close();
+    }
+}
