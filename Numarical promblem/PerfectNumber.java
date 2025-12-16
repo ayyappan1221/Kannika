45081ab154f009c17dbd9093acd26cdce19fea01
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-16 18:33:00 +0530

    Add program to check if a number is a Perfect Number

---
+++ Numarical promblem/PerfectNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == n && n != 0) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not a Perfect Number");
+        }
+        sc.close();
+    }
+}
