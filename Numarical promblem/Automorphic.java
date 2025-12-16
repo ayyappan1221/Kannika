52d12eac5565bdc1007f97dd227400308a165762
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-16 18:38:25 +0530

    Add programs to check if a number is Automorphic and Neon

---
+++ Numarical promblem/Automorphic.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Automorphic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sq = n * n;
+        int temp = n;
+        boolean flag = true;
+
+        while (temp > 0) {
+            if (temp % 10 != sq % 10) {
+                flag = false;
+                break;
+            }
+            temp = temp / 10;
+            sq = sq / 10;
+        }
+
+        if (flag)
+            System.out.println("Automorphic Number");
+        else
+            System.out.println("Not Automorphic Number");
+    }
+}
