e84e1ed3e63764cc1635aa68f3d1d07f79556038
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-17 22:25:03 +0530

    Add CharAtRowInput and StringLength classes for string manipulation

---
+++ UseLengthString.java
@@ -0,0 +1,11 @@
+public class UseLengthString {
+    public static void main(String[] args) {
+
+        String s = "Kannika";
+
+        int len = s.length();
+
+        System.out.println("String: " + s);
+        System.out.println("Length: " + len);
+    }
+}
