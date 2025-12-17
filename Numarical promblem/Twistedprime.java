63a1fcf168fa9d10ce6b370e5f7a2ca299ec2ebc
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-17 20:00:08 +0530

    Add Twisted Prime program to check if a number is a Twisted Prime

---
+++ Numarical promblem/Twistedprime.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Twistedprime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        // check prime
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+
+        if (count != 2) {
+            System.out.println("Not a Twisted Prime");
+            return;
+        }
+
+        // reverse number
+        int temp = n;
+        int rev = 0;
+
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp = temp / 10;
+        }
+
+        int count2 = 0;
+
+        // check prime for reversed number
+        for (int i = 1; i <= rev; i++) {
+            if (rev % i == 0) {
+                count2++;
+            }
+        }
+
+        if (count2 == 2)
+            System.out.println("Twisted Prime");
+        else
+            System.out.println("Not a Twisted Prime");
+
+        sc.close();
+    }
+}
