503f63cebb7d2d198702b9677327a5091913b4bc
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-29 19:16:09 +0530

    12

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.*;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n / 2; i++) {
+            int temp = a[i];
+            a[i] = a[n - 1 - i];
+            a[n - 1 - i] = temp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        sc.close();
+    }
+}
